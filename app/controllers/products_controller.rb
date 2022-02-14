11f245b2c12df141f6cd13c0941e8ff04e7272b4
Igor Areco <igorhareco@gmail.com>
2022-02-14 17:56:03 -0400

    trying to fix inputs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,25 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update]
+
   def edit
 
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :product_type, :rating, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  resources :products, only: %i[edit update]
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :products, only: [:index, :show, :update, :create, :destroy]
